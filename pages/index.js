3dd35b5b43a657b2a9deba29aad528294fdbda7a
Wesley Anjos <wanjos.andrade@gmail.com>
2021-01-11 20:59:02 -0300

    Remove Tailwindcss and Stylized title

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Head from 'next/head'
-import styles from '../styles/home.module.css'
+import Head from "next/head";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>MR Weather</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.home}>
-
+        <div className={styles.logo}>
+          <img src="/images/logo.png" alt="Logo" />
+          <h1 className={styles.title}>
+            MR Weather
+          </h1>
+        </div>
       </main>
-    </div>
-  )
+    </>
+  );
 }
